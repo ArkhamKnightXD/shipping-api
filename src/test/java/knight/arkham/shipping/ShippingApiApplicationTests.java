75940011abb5f0968db8d30d4501bf9bbcbdca51
kevin <karvin_jimenez@hotmail.com>
2022-05-03 22:53:24 -0400

    Proyecto inicializado

---
+++ src/test/java/knight/arkham/shipping/ShippingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package knight.arkham.shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
